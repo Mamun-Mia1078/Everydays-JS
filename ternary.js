4dc2026572288cb8bf8690a80998236261902424
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-05-14 20:55:28 +0600

    4th day practice

---
+++ ternary.js
@@ -0,0 +1,2 @@
+const age = 18;
+console.log('if your age is upper 18 then you are adult, \n  ', age<18 ? 'yes you are adult' : 'No you are not adult');
---
+++ ternary2.js
@@ -0,0 +1,9 @@
+const obj = {
+    name: 'Muzammel Hoque Mamun',
+    age : 23,
+    job: 'Student'
+}
+obj.name ? obj.age ? console.log('it is name and age') :
+ obj.job ? console.log('Yes you are software Engineer') :
+ console.log('no it does not have any job') :
+console.log('does not have any job');
